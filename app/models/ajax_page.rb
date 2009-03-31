b93c6621f90026501cb88a9a5661232b11657c0f
Michael Kessler <michael.kessler@screenconcept.ch>
2009-03-31 13:43:07 +0200

    Changed http request header comparison

--- app/models/ajax_page.rb
+++ app/models/ajax_page.rb
@@ -1,12 +1,15 @@
 class AjaxPage < Page
 
   def render
-    if @request.env['X_REQUESTED_WITH'].casecmp("XmlHttpRequest") > -1 || 
-       @request.env['HTTP_X_REQUESTED_WITH'].casecmp("XmlHttpRequest") > -1
-      
-	render_part(params['part'] || parts.first.name)
+    
+    requested_with = @request.env['X_REQUESTED_WITH'] || @request.env['HTTP_X_REQUESTED_WITH'] || ""
+
+    if requested_with.casecmp("XMLHttpRequest") > -1
+      render_part(params['part'] || parts.first.name)
     else
       super
     end
-  end  
+
+  end
+  
 end
