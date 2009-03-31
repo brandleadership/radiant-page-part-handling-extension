508a32d638ed69f1d2f18769e97c6628a22ab9f7
Michael Kessler <michael.kessler@screenconcept.ch>
2009-03-31 14:37:08 +0200

    Keep it simple stupid! Use Rails xhr? method

--- app/models/ajax_page.rb
+++ app/models/ajax_page.rb
@@ -2,14 +2,12 @@ class AjaxPage < Page
 
   def render
     
-    requested_with = @request.env['X_REQUESTED_WITH'] || @request.env['HTTP_X_REQUESTED_WITH'] || ""
-
-    if requested_with.casecmp("XMLHttpRequest") > -1
+    if request.xhr?
       render_part(params['part'] || parts.first.name)
     else
       super
     end
 
   end
-  
+
 end
